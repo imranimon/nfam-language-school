3c548f840f5d33e185ed983a965d3da5241cd2f3
Imran <imranimon1@gmail.com>
2021-10-05 00:55:28 +0200

    enroll button added

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Row, Spinner } from 'react-bootstrap';
+import swal from 'sweetalert';
 import Course from '../Course/Course';
 import './Services.css'
 
@@ -9,6 +10,23 @@ const Services = () => {
         fetch('./courses.json')
             .then(res => res.json())
             .then(data => setCourses(data)), [])
+
+    // Function for enroll to a course
+    const handleEnroll = () => {
+        swal("Your Name:", {
+            content: "input",
+        })
+            .then((value) => {
+                if (value) {
+                    swal({
+                        title: "You have successfully enrolled yourself !!",
+                        text: `Mr./Mrs: ${value}`,
+                        icon: "success",
+                        button: "Aww Yess!",
+                    });
+                }
+            });
+    }
     return (
         <div className="d-flex justify-content-center">
             {/* Showing spinner when courses are loading */}
@@ -23,7 +41,9 @@ const Services = () => {
                             {/* Showing courses dynamically */}
                             {
                                 courses?.map(course => <Course
-                                    key={course.id} course={course}></Course>)
+                                    key={course.id} course={course}>
+                                    <button onClick={handleEnroll} className="btn btn-outline-secondary">Enroll Yourself</button>
+                                </Course>)
                             }
                         </Row>
                     </div>
