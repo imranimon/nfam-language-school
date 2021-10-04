3cde1ce9c379ac5dbdb4bc10af80caa521b12fbb
Imran <imranimon1@gmail.com>
2021-10-05 00:25:25 +0200

    image added on home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -24,7 +24,8 @@ const Home = () => {
                     <Spinner animation="border" variant="secondary"/>
                 </div>  :
                     <div>
-                        <Row className="p-5 text-center w-100">
+                        <Row className="p-5 pt-0 text-center w-100">
+                            <img className="img-fluid mb-3" src="https://media.istockphoto.com/photos/young-woman-and-man-in-headphones-learning-different-languages-picture-id1032296056?b=1&k=20&m=1032296056&s=170667a&w=0&h=CilP98wuFKV92R5faVBNeeoHRqwfjCMocFPqBedgoCs=" alt="" />
                             <h1 className="text-secondary">Learn a Language. Open Your World</h1>
                             <p>Anytime and anywhere: Learn German online and with our proven quality. No matter whether you are a beginner or an advanced learner no matter whether you want to learn German for professional or private reasons, you will find the right German online course for you.</p>
                         </Row>
